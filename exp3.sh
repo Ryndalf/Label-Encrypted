e205bbf07135fb917536ba9383988a89a328d60c
zrz <132331460+Ryndalf@users.noreply.github.com>
2025-05-17 17:31:51 +1000

    Update exp3.sh

--- exp3.sh
+++ exp3.sh
@@ -5,6 +5,6 @@ file="./source_code/MultiTestM1AndM2.py"
 avg_range=$1
 new_ratio=$2
 
-sed -i '137s/.*/        for i in tqdm.tqdm(range('"$avg_range"')):/' "$file"
+sed -i '139s/.*/        for i in tqdm.tqdm(range('"$avg_range"')):/' "$file"
 
 sed -i '84s/.*/    sample_ratio = '"$new_ratio"'/' "$file"
